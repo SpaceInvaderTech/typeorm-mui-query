165e79bd1ec186fcd53baadaa82c9e43050043e1
Jacob Friis Saxberg <jacob@webcom.dk>
2023-10-16 23:51:20 +0200

    TS

--- src/handleQueryStringParameters.ts
+++ src/handleQueryStringParameters.ts
@@ -11,7 +11,7 @@ const initParameters = {
   offset: 0,
   limit: 100,
   sortModel: [] as GridSortModel,
-  filterModel: undefined as undefined | GridFilterModel,
+  filterModel: { items: [] } as GridFilterModel,
 };
 
 export function handleQueryStringParameters(
